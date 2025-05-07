d482f89afa927f176e64a54eea03b213504dfeac
AvinashKJha2025 <avinash.jha.poc@gmail.com>
2025-05-08 00:58:50 +0530

    Update HelloController.java

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,14 +1,13 @@
 package com.example.controller;
-// TODO: review use of 'EnvConfigUtil'
 import com.example.util.EnvConfigUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 @RestController
 public class HelloController {
     @Autowired
-// TODO: review use of 'EnvConfigUtil'
     private EnvConfigUtil envConfigUtil;
 
         // Non-reactive endpoint
@@ -20,12 +19,8 @@ public class HelloController {
 
         // Reactive endpoint
         @GetMapping("/reactive")
-        // TODO: review conversion from reactive to non-reactive
-        public String reactiveEndpoint() {
-        // TODO: review conversion from reactive to non-reactive
-        String value = envConfigUtil.getProperty("app.name");
-        return "Reactive response: " + value;
+        public Mono<String> reactiveEndpoint() {
+            String value = envConfigUtil.getProperty("app.name");
+            return Mono.just("Reactive response: " + value);
         }
     }
-
-
