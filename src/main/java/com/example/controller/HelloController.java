4fcfe0f409c64346bcc4b2838eefdb99896f7601
AvinashKJha2025 <avinash.jha.poc@gmail.com>
2025-05-08 00:47:28 +0530

    Refactor: added TODOs, non-reactive methods, and test updates

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.example.controller;
+// TODO: review use of 'EnvConfigUtil'
 import com.example.util.EnvConfigUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 @RestController
 public class HelloController {
     @Autowired
+// TODO: review use of 'EnvConfigUtil'
     private EnvConfigUtil envConfigUtil;
 
         // Non-reactive endpoint
@@ -19,9 +20,11 @@ public class HelloController {
 
         // Reactive endpoint
         @GetMapping("/reactive")
-        public Mono<String> reactiveEndpoint() {
-            String value = envConfigUtil.getProperty("app.name");
-            return Mono.just("Reactive response: " + value);
+        // TODO: review conversion from reactive to non-reactive
+        public String reactiveEndpoint() {
+        // TODO: review conversion from reactive to non-reactive
+        String value = envConfigUtil.getProperty("app.name");
+        return "Reactive response: " + value;
         }
     }
 
