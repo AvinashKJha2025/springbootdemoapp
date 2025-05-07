509b708461510ec7f49ea88ccb0a964f859a248f
AvinashKJha2025 <avinash.jha.poc@gmail.com>
2025-05-07 23:37:40 +0530

    Refactor: added TODOs, non-reactive methods, and test updates

--- src/main/java/com/example/util/EnvConfigUtil.java
+++ src/main/java/com/example/util/EnvConfigUtil.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
+// TODO: review use of 'EnvConfigUtil'
 public class EnvConfigUtil {
     @Value("${app.name}")
     private  String appName;
